9d095b894a9050901ff4df48a30d82a9fff5330b
Collins Wekesa <collins.wekesa@andela.com>
2017-03-07 19:59:29 +0300

    modify paystack schema

--- imports/plugins/included/paystack-paymentmethod/lib/collections/schemas/paystack.js
+++ imports/plugins/included/paystack-paymentmethod/lib/collections/schemas/paystack.js
@@ -3,17 +3,17 @@ import { PackageConfig } from "/lib/collections/schemas/registry";
 
 export const PaystackPackageConfig = new SimpleSchema([
   PackageConfig, {
-    "settings.secretKey": {
-      type: Boolean,
-      label: "Secret Key",
-      defaultValue: false,
-      min: 47
-    },
+    // "settings.secretKey": {
+    //   type: Boolean,
+    //   label: "Secret Key",
+    //   defaultValue: false,
+    //   min: 47
+    // },
     "settings.publicKey": {
       type: String,
       label: "Public Key",
-      defaultValue: "Public Key",
-      min: 47
+      min: 47,
+      optional: true
     }
   }
 ]);
